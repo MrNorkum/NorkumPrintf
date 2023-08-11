c852be293aa6082dbeeb111f50b1db90330b3afe
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 22:02:45 +0300

    Successful

--- ft_printf.h
+++ ft_printf.h
@@ -1,20 +1,7 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/10 15:58:56 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/11 14:20:11 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include <unistd.h>
 # include <stdlib.h>
 # include <stdint.h>
 
