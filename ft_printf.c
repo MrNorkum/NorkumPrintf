f41d7357e899595e7aa855d4d521cc5feb951223
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 01:22:36 +0300

    Successful

--- ft_printf.c
+++ ft_printf.c
@@ -1,6 +1,19 @@
 #include "ft_printf.h"
 #include <unistd.h>
 
+int	ft_putstr(char *str)
+{
+	int	len;
+
+	len = -1;
+	if (!str)
+		str = "(null)";
+	while (str[++len])
+		if (write(1, &str[len], 1) == -1)
+			return (-1);
+	return (len);
+}
+
 int	ft_itoa_base_continue(uintptr_t nbr, int base, char *str, int len)
 {
 	int	tab[100];
