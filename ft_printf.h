24edb2c310eb0146e79864067e9a075e6dc58d45
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 01:25:12 +0300

    Successful

--- ft_printf.h
+++ ft_printf.h
@@ -2,12 +2,7 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include <stdint.h>
 
 int		ft_printf(const char *str, ...);
-int		ft_putchar(char c);
-int		ft_putstr(char *str);
-int		ft_itoa_base(uintptr_t nbr, int base, char *str, int mod);
-int		ft_format(va_list args, char format);
 
 #endif
