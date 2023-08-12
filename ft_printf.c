e6917f4c21c9a90381e5a5cfd4b074b92910b9b1
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 01:19:05 +0300

    Successful

--- ft_printf.c
+++ ft_printf.c
@@ -1,6 +1,31 @@
 #include "ft_printf.h"
 #include <unistd.h>
 
+int	ft_itoa_base(uintptr_t nbr, int base, char *str, int mod)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	if (mod == 1 && (int)nbr < 0)
+	{
+		nbr *= -1;
+		if (write(1, "-", 1) == -1)
+			return (-1);
+		len++;
+	}
+	if (mod == 2)
+	{
+		if (ft_putstr("0x") == -1)
+			return (-1);
+		len += 2;
+	}
+	if (nbr == 0)
+		return (len + write(1, "0", 1));
+	return (ft_itoa_base_continue(nbr, base, str, len));
+}
+
 int	ft_format(va_list args, char format)
 {
 	if (format == 'c')
